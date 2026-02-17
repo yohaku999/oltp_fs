104445ef436ce8d6b6d9ea9af23be008f4e56e04
yohaku999 <yohaku.ou@gmail.com>
2026-02-17 20:37:02 +0900

    add some test for page

--- test/page.cpp
+++ test/page.cpp
@@ -1,8 +1,9 @@
 #include "../src/page.h"
 #include "../src/cell.h"
+#include <array>
 #include <cassert>
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 class PageTest
 {
@@ -13,16 +14,32 @@ public:
         std::cout << "All Page tests passed!" << std::endl;
     }
 
+    // TODO: insert till fails
     void testInsertLeafPageAndFind()
     {
-        LeafCell cell(11111, 999, 15);
         char page_data[Page::PAGE_SIZE_BYTE] = {};
         Page *page = Page::initializePage(page_data, true);
-        int slot_id = page->insertCell(cell);
-        assert(slot_id == 0);
-        std::pair<uint16_t, uint16_t> ref = page->findLeafRef(11111);
-        assert(ref.first == 999);
-        assert(ref.second == 15);
+        struct Entry
+        {
+            int key;
+            uint16_t heap_page_id;
+            uint16_t slot_id;
+        } entries[] = {
+            {11111, 999, 15},
+            {22222, 500, 2},
+            {33333, 123, 7}
+        };
+
+        for (int i = 0; i < static_cast<int>(std::size(entries)); ++i)
+        {
+            const Entry &entry = entries[i];
+            int slot_id = page->insertCell(LeafCell(entry.key, entry.heap_page_id, entry.slot_id));
+            assert(slot_id == i);
+
+            std::pair<uint16_t, uint16_t> ref = page->findLeafRef(entry.key);
+            assert(ref.first == entry.heap_page_id);
+            assert(ref.second == entry.slot_id);
+        }
         std::cout << "testInsertLeafPageAndFind: OK" << std::endl;
     }
 };
