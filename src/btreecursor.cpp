7e59129be3d47952e44d433d77203ff0fde6c232
yohaku999 <yohaku.ou@gmail.com>
2026-02-21 14:18:55 +0900

    feat : add update api also update each crud logic to handle the case when key doesn't exists properly.

--- src/btreecursor.cpp
+++ src/btreecursor.cpp
@@ -26,28 +26,47 @@ int BTreeCursor::findLeafPageID(BufferPool& pool, File& indexFile, int key)
     return pageID;
 }
 
-std::pair<uint16_t, uint16_t> BTreeCursor::findRecordLocation(BufferPool& pool, File& indexFile, int key, bool do_invalidate)
+std::optional<std::pair<uint16_t, uint16_t>> BTreeCursor::findRecordLocation(BufferPool& pool, File& indexFile, int key, bool do_invalidate)
 {
-    // TODO: do not invalidate index nodes during traversal for now. we will come back to this when we start to support concurrency.
+    // NOTE: we decided not to invalidate intermediate nodes during traversal for now.
+    // we will come back to this when we start to support concurrency.
     int pageID = findLeafPageID(pool, indexFile, key);
     Page *leafPage = pool.getPage(pageID, indexFile);
-    auto ref = leafPage->findLeafRef(key);
-    if (!ref.has_value())
-    {
-        throw std::runtime_error("Key " + std::to_string(key) + " not found in leaf page.");
-    }
-    return ref.value();
+    return leafPage->findLeafRef(key, do_invalidate);
+}
+
+void BTreeCursor::update(BufferPool& pool, File& indexFile, File& heapFile, int key, char* value, size_t value_size)
+{
+    /**
+     * We first design updates to be idempotent by modeling them as a remove followed by an insert.
+     * This is not necessarily the most efficient strategy, but it keeps the update path simple and robust and fasten the development.
+     * Also, this unlocks follow-on benefits (e.g., easier recovery/retry and fewer page-structure assumptions) without requiring 
+     * in-place updates or special-case split handling.
+     */
+    BTreeCursor::remove(pool, indexFile, heapFile, key);
+    BTreeCursor::insert(pool, indexFile, heapFile, key, value, value_size);
 }
 
 char* BTreeCursor::read(BufferPool& pool, File& indexFile, File& heapFile, int key)
 {
-    auto [pageID, slotID] = findRecordLocation(pool, indexFile, key);
+    auto location = findRecordLocation(pool, indexFile, key);
+    if (!location.has_value())
+    {
+        throw std::runtime_error("Key " + std::to_string(key) + " not found in leaf page.");
+    }
+    auto [pageID, slotID] = location.value();
     Page *page = pool.getPage(pageID, heapFile);
     return page->getXthSlotValue(slotID);
 }
 
 void BTreeCursor::remove(BufferPool& pool, File& indexFile, File& heapFile, int key)
-{   auto [pageID, slotID] = findRecordLocation(pool, indexFile, key, true);
+{
+    auto location = findRecordLocation(pool, indexFile, key, true);
+    if (!location.has_value())
+    {
+        throw std::runtime_error("Key " + std::to_string(key) + " not found in leaf page.");
+    }
+    auto [pageID, slotID] = location.value();
     Page *page = pool.getPage(pageID, heapFile);
     page->invalidateSlot(slotID);
     spdlog::info("Removed record with key {} successfully.", key);
@@ -55,13 +74,15 @@ void BTreeCursor::remove(BufferPool& pool, File& indexFile, File& heapFile, int 
 
 void BTreeCursor::insert(BufferPool& pool, File& indexFile, File& heapFile, int key, char* value, size_t value_size)
 {
-    int leaf_page_id = findLeafPageID(pool, indexFile, key);
-    Page *leafPage = pool.getPage(leaf_page_id, indexFile);
-    if (leafPage->hasKey(key))
+    spdlog::info("Inserting record with key {} into index {}, heap {}", key, indexFile.getFilePath(), heapFile.getFilePath());
+    // check if valid key already exists.
+    auto location = findRecordLocation(pool, indexFile, key);
+    if (location.has_value())
     {
-        throw std::logic_error("The key " + std::to_string(key) + " already exists.");
+        throw std::runtime_error("Key " + std::to_string(key) + " already exists. Duplicate keys are not allowed.");
     }
 
+    // insert to the heap file
     RecordCell cell(key, value, value_size);
     int targetPageID = heapFile.getMaxPageID();
     Page *heapPage = pool.getPage(targetPageID, heapFile);
@@ -78,6 +99,9 @@ void BTreeCursor::insert(BufferPool& pool, File& indexFile, File& heapFile, int 
     }
     spdlog::info("Inserted record with key {} into heap page ID {} successfully.", key, targetPageID);
 
+    // find the leaf page to insert the new record.
+    int leaf_page_id = findLeafPageID(pool, indexFile, key);
+    Page *leafPage = pool.getPage(leaf_page_id, indexFile);
     auto leaf_slot_id = leafPage->insertCell(LeafCell(key, targetPageID, insertedSlotID.value()));
     if (!leaf_slot_id.has_value())
     {
--- src/btreecursor.h
+++ src/btreecursor.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <optional>
 #include <string>
 #include <utility>
 #include "bufferpool.h"
@@ -15,10 +16,12 @@
 class BTreeCursor
 {
 private:
-    static std::pair<uint16_t, uint16_t> findRecordLocation(BufferPool& pool, File& indexFile, int key, bool do_invalidate=false);
+    static std::optional<std::pair<uint16_t, uint16_t>> findRecordLocation(
+        BufferPool& pool, File& indexFile, int key, bool do_invalidate=false);
     static int findLeafPageID(BufferPool& pool, File& indexFile, int key);
 public:
     static char* read(BufferPool& pool, File& indexFile, File& heapFile, int key);
     static void insert(BufferPool& pool, File& indexFile, File& heapFile, int key, char* value, size_t value_size);
     static void remove(BufferPool& pool, File& indexFile, File& heapFile, int key);
+    static void update(BufferPool& pool, File& indexFile, File& heapFile, int key, char* value, size_t value_size);
 };
